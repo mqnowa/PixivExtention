f055680325d8e7e4919de53a87abb6364dce9b4b
Avent <44436695+abeckent@users.noreply.github.com>
2022-11-04 19:10:21 +0900

    asdasd

--- prexiview.js
+++ prexiview.js
@@ -78,7 +78,7 @@ async function apply_script() {
 
 
 
-function main() {
+function addPreviewModeButton() {
     const link = document.createElement("link")
     link.rel = "stylesheet"
     link.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
